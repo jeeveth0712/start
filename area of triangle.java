59462311ec64f632bdebf8382615d7b4941acb00
Jeeveth <jeevethkoka@gmail.com>
2021-02-23 22:26:08 +0530

    Adding my first java file :(

---
+++ area of triangle.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main{
+    static double area(double b,double h){
+        double a=(b*h)/2;
+        return a;
+    }
+    public static void main(String[]args){
+        double base,height;
+        Scanner sc=new Scanner(System.in);
+        base=sc.nextDouble();
+        height=sc.nextDouble();
+        double ar=area(base,height);
+        System.out.printf("%.1f",ar);
+    }
+}
